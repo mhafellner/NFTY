5eee077e3b86df4d601df8d1ef67374ee8d8a1a3
JonnysCode <jonathangruss@hotmail.com>
2021-11-23 13:43:38 +0100

    contracts from fractional

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,10 @@
 const Migrations = artifacts.require("Migrations");
+const Settings = artifacts.require("fractional/Settings");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+};
+
+module.exports = function (deployer) {
+  deployer.deploy(Settings);
 };
