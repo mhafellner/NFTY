e6a305d3f04d116dd1874754ecd7661e9dfcb19f
JonnysCode <jonathangruss@hotmail.com>
2021-11-23 16:08:47 +0100

    Remove unnecessary migration contracts

--- migrations/2_fractional_migration.js
+++ migrations/2_fractional_migration.js
@@ -1,15 +1,10 @@
 const Settings = artifacts.require("Settings");
-const InitializedProxy = artifacts.require("InitializedProxy");
-const TokenVault = artifacts.require("TokenVault");
 const ERC721VaultFactory = artifacts.require("ERC721VaultFactory");
 
 
 module.exports = function (deployer) {
   deployer.deploy(Settings).then( async () => {
     let settingsInstance = await Settings.deployed();
-    deployer.deploy(TokenVault, settingsInstance);
-    deployer.deploy(ERC721VaultFactory, settingsInstance);
-
-    //deployer.deploy(InitializedProxy);
+    deployer.deploy(ERC721VaultFactory, settingsInstance.address);
   });
 };
