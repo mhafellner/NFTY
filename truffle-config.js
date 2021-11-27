35dee34767ab20d8246299c829bde396d1e7d04c
Armin Satzger <armin.satzger@gmail.com>
2021-11-27 16:26:42 +0100

    Add Kovan test network configs, Etherscan verification

--- truffle-config.js
+++ truffle-config.js
@@ -80,16 +80,29 @@ module.exports = {
 
     goerli: {
         provider: () => new HDWalletProvider(
-            process.env.GOERLI_MNEMONIC,
+            process.env.MNEMONIC,
             process.env.GOERLI_PROVIDER_URL,
         ),
         network_id: 5, // Goerli's id
         gas: GAS_LIMIT,
         gasPrice: 10e9, // 10 GWEI
         //confirmations: 6, // # of confs to wait between deployments. (default: 0)
-        timeoutBlocks: 50, // # of blocks before a deployment times out  (minimum/default: 50)
+        timeoutBlocks: 500, // # of blocks before a deployment times out  (minimum/default: 50)
         skipDryRun: false // Skip dry run before migrations? (default: false for public nets )
     },
+
+    kovan: {
+      provider: () => new HDWalletProvider(
+          process.env.MNEMONIC,
+          process.env.KOVAN_PROVIDER_URL,
+      ),
+      network_id: 42, // Goerli's id
+      gas: GAS_LIMIT,
+      gasPrice: 10e9, // 10 GWEI
+      //confirmations: 6, // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 500, // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: false // Skip dry run before migrations? (default: false for public nets )
+  },
 
     ganache: {
         host: "127.0.0.1",
@@ -118,4 +131,13 @@ module.exports = {
       // }
     },
   },
+
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_API_KEY
+  }
+  
 };
